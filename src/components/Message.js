30567a6e8262e949d9ddef85356a2264aba5d038
Joscelyn James <josce.james7@hotmail.com>
2017-10-11 15:38:51 -0600

    show if message is checked/selected

--- src/components/Message.js
+++ src/components/Message.js
@@ -10,26 +10,36 @@ class Message extends React.Component {
 		}
 	}
 
-	getReadStatus (email) {
-			if (email.read === true) {
-				console.log(true)
-				return 'row message read';
-			} else {
-				console.log(false)
-				return 'row message unread';
-			}
+	getReadStatus(email) {
+		if (email.read === true) { 
+			return 'read';
+		} else { 
+			return 'unread';
+		}
 	};
+
+	getCheckStatus(email) {
+		if (email.selected === true) {
+			return 'checked';
+		} 
+	};
+
+	getSelectStatus(email) {
+		if (email.selected === true) {
+			return 'selected'
+		}
+	}
 
 	// uread messge
 	render () {
 		return (
 			this.props.emails.map((email) => {
 				return (
-					<div className={this.getReadStatus(email)} key={email.id}>
+					<div className={"row message " + this.getReadStatus(email) + " " + this.getSelectStatus(email)} key={email.id}>
 						<div className="col-xs-1">
 							<div className="row">
 								<div className="col-xs-2">
-									<input type="checkbox" />
+									<input type="checkbox" checked={this.getCheckStatus(email)}/>
 								</div>
 								<div className="col-xs-2">
 									<i className="star fa fa-star"></i>
