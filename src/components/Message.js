f122e29cfd9b58c45686d151556df7b5511de348
Joscelyn James <josce.james7@hotmail.com>
2017-10-11 15:16:27 -0600

    shows if message is read or unread

--- src/components/Message.js
+++ src/components/Message.js
@@ -4,33 +4,48 @@ class Message extends React.Component {
 	constructor(props) {
 		super(props)
 
-		this.state = { }
+		this.state = { 
+			read: '',
+
+		}
 	}
+
+	getReadStatus (email) {
+			if (email.read === true) {
+				console.log(true)
+				return 'row message read';
+			} else {
+				console.log(false)
+				return 'row message unread';
+			}
+	};
+
 	// uread messge
 	render () {
 		return (
 			this.props.emails.map((email) => {
 				return (
-				<div className="row message read"  key={email.id}>
-					<div className="col-xs-1">
-						<div className="row">
-							<div className="col-xs-2">
-								<input type="checkbox" />
-							</div>
-							<div className="col-xs-2">
-								<i className="star fa fa-star"></i>
+					<div className={this.getReadStatus(email)} key={email.id}>
+						<div className="col-xs-1">
+							<div className="row">
+								<div className="col-xs-2">
+									<input type="checkbox" />
+								</div>
+								<div className="col-xs-2">
+									<i className="star fa fa-star"></i>
+								</div>
 							</div>
 						</div>
-					</div>
-					<div className="col-xs-11" >
-						<a href="#">
-							{email.subject}
-						</a>
-					</div>
-			</div>
+						<div className="col-xs-11" >
+							<a href="#">
+								{email.subject}
+							</a>
+						</div>
+				</div>
 			)
 		})
 	)}
 };
+
 
 export default Message;
