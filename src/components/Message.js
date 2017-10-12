24f0fe55da083a50359ce6b2e0722abad6ee7a43
Joscelyn James <josce.james7@hotmail.com>
2017-10-12 10:21:21 -0600

    can now check and uncheck boxes and thye highlight accordingly

--- src/components/Message.js
+++ src/components/Message.js
@@ -4,7 +4,9 @@ class Message extends React.Component {
 	constructor(props) {
 		super(props)
 
-		this.state = { }
+		this.state = { 
+			  
+		}
 
 		this.handleClick = this.handleClick.bind(this);
 	}
@@ -19,17 +21,33 @@ class Message extends React.Component {
 
 	getCheckStatus(email) {
 		if (email.selected === true) {
-			return true;
-		} else {
-			return false;
-		}
+			return 'checked';
+		} 
 	};
 
 	getSelectStatus(email) {
+		console.log('merp')
 		if (email.selected === true) {
-			return 'selected'
+			return 'selected';
+		} else {
+			email.selected = false;
+			return ''
 		}
+		this.setState({})
 	};
+
+	updateSelect(email) {
+		console.log(email.id)
+		let id = email.id
+		if (email.selected === false) {
+			email.selected = true;
+			console.log(email)
+		} else {
+			email.selected = false;
+			console.log(email)
+		}
+		this.setState({ id: email.selected })
+	}
 
 	getLabels(labels) {
 		return (
@@ -56,7 +74,7 @@ class Message extends React.Component {
 	 	} else {
 			email.starred = true;
 		}
-
+		// had to use this to update* or fun render() again to update. It just refreshes the state but dosent set it to anything 
 		this.setState({});
 	};
 
@@ -68,7 +86,7 @@ class Message extends React.Component {
 						<div className="col-xs-1">
 							<div className="row">
 								<div className="col-xs-2">
-									<input type="checkbox" onChange={() => this.getCheckStatus(email)}/>
+									<input type="checkbox" checked={this.getCheckStatus(email)} onChange={() => this.updateSelect(email)}/>
 								</div>
 								<div onClick={(e) => this.handleClick(e, email)} className="col-xs-2">
 									<i className={this.getStarStatus(email)}></i>
